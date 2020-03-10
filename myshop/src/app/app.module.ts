39b1561eb8d1fe82a5f0e90970373cf24c5440f3
Jarosław Lucega <jl28180@zpsb.pl>
2020-03-10 13:09:44 +0100

    Dodano routing, landing i products

--- myshop/src/app/app.module.ts
+++ myshop/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
+
+const appRoutes: Routes = [
+{ path: '', redirectTo: '/landing', pathMatch: 'full' },
+{ path: 'landing', component: LandingComponent },
+{ path: 'products', component: ProductsComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
